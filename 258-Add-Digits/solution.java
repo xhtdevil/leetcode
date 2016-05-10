32c2228be6893fa5d51800465013e56cc2600aa9
xhtdevil <xuhaotian201314@yahoo.com>
2016-05-10 23:01:26 +0800

    [258. Add Digits][Time Limit Exceeded]committed by LeetCode Extension

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -2,11 +2,11 @@ public class Solution {
     public int addDigits(int num) {
         int newNum = 0;
         while(num / 10 != 0){
-            while(num){
+            while(num != 0){
                 newNum += num % 10;
                 num = num / 10;
             }
-            num = newNum
+            num = newNum;
         }
         return num;
     }
