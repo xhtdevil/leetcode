15cba67724a44cebab5be8d63dbce41a6d22016b
xhtdevil <xuhaotian201314@yahoo.com>
2016-05-11 10:25:17 +0800

    [258. Add Digits][Accepted]committed by LeetCode Extension

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -1,13 +1,6 @@
 public class Solution {
     public int addDigits(int num) {
-        int newNum = 0;
-        while(num / 10 != 0){
-            while(num != 0){
-                newNum += num % 10;
-                num = num / 10;
-            }
-            num = newNum;
-        }
-        return num;
+       
+        return  (num-1)%9+1;//find the rule
     }
 }
