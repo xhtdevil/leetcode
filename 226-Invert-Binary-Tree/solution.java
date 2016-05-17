1b2ebc8d310078978fd6b9abbd4d9ac82ea62b9e
xhtdevil <xuhaotian201314@yahoo.com>
2016-05-17 16:55:38 +0800

    [226. Invert Binary Tree][Compile Error]committed by LeetCode Extension

---
+++ 226-Invert-Binary-Tree/solution.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        TreeNode node = TreeNode(root);
+        if(node != null){
+            node.left = invertTree(root.right);
+            node.right = invertTree(root.left);
+            return node;
+        }else{
+            return node;
+        }
+    }
+}
