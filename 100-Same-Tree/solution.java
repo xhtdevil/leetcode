4e888a20511f25be1c374777c4a92499dff86e41
xhtdevil <xuhaotian201314@yahoo.com>
2016-05-18 22:17:26 +0800

    [100. Same Tree][Runtime Error]committed by LeetCode Extension

---
+++ 100-Same-Tree/solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){return true;}
+        if(p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right)){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
