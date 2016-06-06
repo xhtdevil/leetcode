03ab0dc6efac7d9c1a9e3ae1cbd4ff55d093fe69
xhtdevil <xuhaotian201314@yahoo.com>
2016-06-06 20:53:11 +0800

    [24. Swap Nodes in Pairs][Runtime Error]committed by LeetCode Extension

---
+++ 24-Swap-Nodes-in-Pairs/solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode next = null;
+        ListNode node = head;
+        head = head.next;
+        while(node != null && node.next != null){
+            next = node.next.next;
+            node.next.next = node;
+            if(next != null && next.next != null){
+                node.next = next.next;
+            }else if(next != null && next.next == null){
+                node.next = next;
+            }else{node.next = null;} 
+            node = next;
+        }
+        return head;
+    }
+}
