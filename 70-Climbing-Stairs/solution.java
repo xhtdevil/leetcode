662e5ef47f7b76234b926b3b5b4522f123f9b2b6
xhtdevil <xuhaotian201314@yahoo.com>
2016-06-06 13:32:16 +0800

    [70. Climbing Stairs][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 70-Climbing-Stairs/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if(n == 1){return 1;}
+        else if(n == 2){return 2;}
+        else{
+            return climbStairs(n - 2) + climbStairs(n - 1);
+        }
+    }
+}
