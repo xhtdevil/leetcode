ff6ec26fb0a5f55c0eedbafa83d68c4023c05a4b
xhtdevil <xuhaotian201314@yahoo.com>
2016-06-06 20:26:18 +0800

    [21. Merge Two Sorted Lists][Accepted]committed by LeetCode Extension

--- 21-Merge-Two-Sorted-Lists/solution.java
+++ 21-Merge-Two-Sorted-Lists/solution.java
@@ -11,11 +11,12 @@ public class Solution {
         ListNode node1 = l1;
         ListNode node2 = l2;
         ListNode head = null, node3 = head;
-        if(node1 == null && node2 == null){
-            head = null;
-            return head;
+        if(l1 == null && l2 == null){                           //find the head of merged list
+            return null;
         }
-        if((node1 != null && node2 == null) || node1.val <  node2.val){//find the head of merged list
+        else if(l1 != null && l2 == null){return l1;}
+        else if(l1 == null && l2 != null){return l2;}
+        else if(node1.val <  node2.val){
             node3 = node1;
             head = node1;
             node1 = node1.next;
@@ -24,7 +25,7 @@ public class Solution {
             head = node2;
             node2 = node2.next;
         }
-        while(node1 != null && node2 != null){
+        while(node1 != null && node2 != null){                  //link and merge two lists according to val
             if(node1.val <  node2.val){
                 node3.next = node1;
                 node1 = node1.next;
