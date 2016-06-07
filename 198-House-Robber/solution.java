df7ad64a34d682daae40621bbc38f2ac509998e3
xhtdevil <xuhaotian201314@yahoo.com>
2016-06-07 08:57:40 +0800

    [198. House Robber][Accepted]committed by LeetCode Extension

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int rob(int[] nums) {
-        if(nums == null){return 0;}
+        if(nums.length == 0){return 0;}
         else if(nums.length == 1){return nums[0];}
         else if(nums.length == 2){return Math.max(nums[0],nums[1]);}
         else {
