c49e78d1c866a02e2e64dd0733b1a55c2479dbde
xhtdevil <xuhaotian201314@yahoo.com>
2016-07-27 22:52:25 +0800

    [190. Reverse Bits][Compile Error]committed by LeetCode Extension

---
+++ 190-Reverse-Bits/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        if(n < 0){long m = 0x100000000L + n}
+        else{long m = (long) n;}
+        int reverse = 0;
+        for(int i = 0; i < 32; i++){
+            reverse = (int) (2 * reverse + m % 2);
+            m = m / 2;
+        }
+        return reverse;
+    }
+}
