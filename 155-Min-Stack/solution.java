d29d03b119b407d7d3393b7317468da0aad75fa1
xhtdevil <xuhaotian201314@yahoo.com>
2016-07-28 17:49:26 +0800

    [155. Min Stack][Accepted]committed by LeetCode Extension

--- 155-Min-Stack/solution.java
+++ 155-Min-Stack/solution.java
@@ -1,5 +1,8 @@
 public class MinStack {
-    // notice the 
+    
+    // time: O(1),use one stack
+    int min = Integer.MAX_VALUE;
+    
     private Stack<Integer> stack;
     /** initialize your data structure here. */
     public MinStack() {
@@ -7,35 +10,46 @@ public class MinStack {
     }
     
     public void push(int x) {
+        if(min >= x){           //must include "=" here
+            stack.push(min);    //push both x and oldmin if min changes
+            min = x;
+        }
         stack.push(x);
     }
     
     public void pop() {
-        if(stack.isEmpty())return;
+        if(stack.isEmpty()){min = Integer.MAX_VALUE;return;};
+        if(top() == min){
+            stack.pop();
+            min = stack.peek();
+            stack.pop();
+        }
         else stack.pop();
     }
     
     public int top() {
-         if(stack.isEmpty())return -10000;
+         if(stack.isEmpty())return 0;
         else return stack.peek();
     }
     
-    public int getMin() {               //must recover the stack after find min
-        int min = top();
-        Stack<Integer> temp = new Stack<Integer>();
-        temp.push(top());
-        pop();
-        while(!stack.isEmpty()){
-            if(top() < min){
-                min = top();
-            }
-            temp.push(top());
-            pop();
-        }
-        while(!temp.isEmpty()){
-            push(temp.pop());
-        }
+    public int getMin() { 
         return min;
+        //must recover the stack after find min
+        // int min = top();
+        // Stack<Integer> temp = new Stack<Integer>();
+        // temp.push(top());
+        // pop();
+        // while(!stack.isEmpty()){
+        //     if(top() < min){
+        //         min = top();
+        //     }
+        //     temp.push(top());
+        //     pop();
+        // }
+        // while(!temp.isEmpty()){
+        //     push(temp.pop());
+        // }
+        // return min;
     }
 }
 
