23d1a82faa246a6299d6a4ab02e8bf141e501c96
xhtdevil <xuhaotian201314@yahoo.com>
2016-08-03 21:44:04 +0800

    [122. Best Time to Buy and Sell Stock II][Accepted]committed by LeetCode Extension

--- 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
@@ -1,23 +1,33 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        int buyPrice = 0, sellPrice = 0, profit = 0;
         int n = prices.length;
         if(n <= 1)return 0;
-        for(int i = 0; i < n; i++){
-            if(i == 0 && prices[i + 1] > prices[i]){
-                buyPrice = prices[i];
+        int buyPrice = 0, sellPrice = 0, profit = 0;
+        int asc = 0, lastPrice = prices[0];
+        // if(prices[0] > prices[1]){
+        //     asc = -1;
+        // }else if(prices[0] < prices[1]){
+        //     buyPrice = prices[0];
+        //     asc = 1;
+        // }
+        
+        for(int i = 1; i < n; i++){
+            if(prices[i] < lastPrice && asc != -1){
+                if(asc != 0){
+                    sellPrice = lastPrice;
+                    profit += sellPrice - buyPrice;
+                }
+                asc = -1;
             }
-            else if(i == n - 1 && prices[i - 1] < prices[i]){
-                sellPrice = prices[i];
-                profit += sellPrice - buyPrice;
+            else if(prices[i] > lastPrice && asc != 1){
+                buyPrice = lastPrice;
+                asc = 1;
             }
-            else if(prices[i] < prices[i - 1] && prices[i] < prices[i + 1]){
-                buyPrice = prices[i];
-            }
-            else if(prices[i] > prices[i + 1] && prices[i] > prices[i - 1]){
-                sellPrice = prices[i];
-                profit += sellPrice - buyPrice;
-            }
+            lastPrice = prices[i];
+        }
+        if(asc == 1){
+            sellPrice = prices[n - 1];
+            profit += sellPrice - buyPrice;
         }
         return profit;
     }
