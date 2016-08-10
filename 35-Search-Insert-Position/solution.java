5e4538c9ca3678fad4782611b86ebe670475993f
xhtdevil <xuhaotian201314@yahoo.com>
2016-08-10 11:36:47 +0800

    [35. Search Insert Position][Accepted]committed by LeetCode Extension

--- 35-Search-Insert-Position/solution.java
+++ 35-Search-Insert-Position/solution.java
@@ -10,7 +10,7 @@ public class Solution {
                 return helper(nums, target, mid + 1, right);
             }
             else if(target < nums[mid]){
-                return helper(nums, target, left, mid - 1);
+                return helper(nums, target, left, mid);
             }
             else{
                 return mid;
