ddb031941f3efb9e14497f0c42d675f356f1c223
xhtdevil <xuhaotian201314@yahoo.com>
2016-08-12 12:38:36 +0800

    [62. Unique Paths][Time Limit Exceeded]committed by LeetCode Extension

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -3,20 +3,25 @@ public class Solution {
     private HashMap<int[], Integer> map = new HashMap<int[], Integer>();
     
     public int uniquePaths(int m, int n) {
-        if(m == 1 && n == 1)return 1;
+        if(m == 1 && n == 1)return 1; 
+        int[] loc = {m, n};
+        int[] locRev = {n, m};
         int ans = 0;
+        if(map.containsKey(loc) || map.containsKey(locRev)){
+            ans = map.get(loc);
+            return ans;
+        }
+
         int upperPaths = 0;
         int leftPaths = 0;
+        
         if(m > 1){
             upperPaths = uniquePaths(m - 1, n);
         }
         if(n > 1){
             leftPaths = uniquePaths(m, n - 1);
         }
-        int[] loc = {m, n};
-        int[] locRev = {n, m};
-        if(map.containsKey(loc) || map.containsKey(locRev))ans = map.get(loc);
-        else ans = upperPaths + leftPaths;
+        ans = upperPaths + leftPaths;
         map.put(loc, ans);
         map.put(locRev, ans);
         return ans;
