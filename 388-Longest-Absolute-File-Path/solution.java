ea0a502b849c67170c646870ea2318f65979747d
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-02 21:20:07 +0800

    [388. Longest Absolute File Path][Runtime Error]committed by LeetCode Extension

---
+++ 388-Longest-Absolute-File-Path/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+
+    public int lengthLongestPath(String input) {
+        int max = 0;
+        int[] longestPath = new int[input.length()];
+        String[] path = input.split("\n");
+        longestPath[0] = 0;
+        for(int i = 0; i < path.length; i++){
+            int level = 0;
+            int l = path[i].length();
+            int j = 0;
+             while(j < path[i].length() && path[i].charAt(j) == '\t'){
+                 level++;
+                 j++;
+                 l--;
+             }
+            if(path[i].contains(".")){
+                max = Math.max(max, longestPath[level] + l);
+            }
+            else{
+                longestPath[level + 1] = longestPath[level] + l + 1;
+            }
+        }
+        return max;
+    }
+}
