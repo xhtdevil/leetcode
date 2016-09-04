0534ecd40ae4eb82b7a4351800369d52f2618f79
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-04 23:34:41 +0800

    [69. Sqrt(x)][Time Limit Exceeded]committed by LeetCode Extension

--- 69-Sqrt(x)/solution.java
+++ 69-Sqrt(x)/solution.java
@@ -1,14 +1,16 @@
 public class Solution {
     public int mySqrt(int x) {
+        if(x == 0)return 0;
+        if(x == 1)return 1;
         int l = 0, r = x;
         int mid = l + (x - l) / 2;
         while(l <= r){
             mid = l + (x - l) / 2;
-            if(mid < x / mid){
-                l = mid + 1;
-            }else{
-                if((mid + 1) < x / (mid + 1))return mid;
+            if((mid != 0 && mid > x / mid)){
                 r = mid - 1;
+            }else{
+                if((mid + 1) > x / (mid + 1))return mid;
+                l = mid + 1;
             }
         }
         return 0;
