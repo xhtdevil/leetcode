d85025dfe158b18b7d050812c158c653b63af86e
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-04 23:02:35 +0800

    [50. Pow(x, n)][Runtime Error]committed by LeetCode Extension

--- 50-Pow(x,-n)/solution.java
+++ 50-Pow(x,-n)/solution.java
@@ -1,16 +1,14 @@
 public class Solution {
     public double myPow(double x, int n) {
-        double mul = 1;
-        if(n >= 0){
-            for(int i = 0; i < n; i++){
-                mul *= x;
-            }
+        //double mul = 1;
+        if(n == 0)return 1;
+        else if(n > 0){
+            if(n % 2 == 0)return myPow(x * x, n / 2);
+            else    return x * myPow(x * x, (n - 1) / 2); 
         }
         else{
-            for(int i = 0; i < Math.abs(n); i++){
-                mul /= x;
-            }
+            if(Math.abs(n) % 2 == 0)return myPow(x * x, n / 2);
+            else    return myPow(x * x, (n - 1) / 2) / x; 
         }
-        return mul;
     }
 }
