2bfd891afc353128267a8755ba68a1ca79bc6a93
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-07 14:02:14 +0800

    [322. Coin Change][Time Limit Exceeded]committed by LeetCode Extension

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -1,33 +1,37 @@
 public class Solution {
     private int nums = 0;
-    private int min = Integer.MAX_VALUE;
+    // private int min = Integer.MAX_VALUE;
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
     public int coinChange(int[] coins, int amount) {
         if(coins.length == 0)return -1;
         Arrays.sort(coins);
-        
-        // if(min == Integer.MAX_VALUE){
-        //     return -1;
-        // }
-        // else return min;
         return helper(coins, amount);
     }
     
     private int helper(int[] coins, int amount){
-        // if(amount == 0 && nums < min){
-        //     min = nums;
-        //     return;
-        // }
         if(amount == 0)return nums;
-        if(amount < 0)return -1;
+        if(map.containsKey(amount))return map.get(amount);
         for(int i = coins.length - 1; i >= 0; i--){
+            if(coins[i] > amount)continue;
             int maxNum = amount / coins[i];
             for(int j = maxNum; j > 0; j--){
                 nums += j;
                 int ans = helper(coins, amount - j * coins[i]);
-                if(ans != -1)return ans;
+                if(ans != -1 ){
+                    if(!map.containsKey(amount) || map.get(amount) > ans){
+                        map.put(amount, ans);
+                        
+                    }
+                }
                 nums -= j;
             }
         }
-        return -1;
+        if(!map.containsKey(amount)){
+            map.put(amount, -1);
+            return -1;
+        }else{
+            return map.get(amount);
+        }
+        
     }
 }
