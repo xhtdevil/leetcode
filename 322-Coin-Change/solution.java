06093667f67ac29f9a650a6819032edc1040a474
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-07 14:51:29 +0800

    [322. Coin Change][Time Limit Exceeded]committed by LeetCode Extension

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -1,37 +1,50 @@
 public class Solution {
     private int nums = 0;
     // private int min = Integer.MAX_VALUE;
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    //Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    
     public int coinChange(int[] coins, int amount) {
         if(coins.length == 0)return -1;
-        Arrays.sort(coins);
-        return helper(coins, amount);
+        //Arrays.sort(coins);
+        int[] map = new int[amount + 1];
+        for(int i = 0; i < amount + 1; i++){
+            map[i] = 0;
+        }
+        helper(coins, amount, map);
+        return map[amount];
     }
     
-    private int helper(int[] coins, int amount){
-        if(amount == 0)return nums;
-        if(map.containsKey(amount))return map.get(amount);
+   
+    
+    private void helper(int[] coins, int amount, int[] map){
+        if(amount == 0 || map[amount] != 0)return;
+       // if(map[amount] != 0)return map[amount];
         for(int i = coins.length - 1; i >= 0; i--){
             if(coins[i] > amount)continue;
             int maxNum = amount / coins[i];
             for(int j = maxNum; j > 0; j--){
-                nums += j;
-                int ans = helper(coins, amount - j * coins[i]);
-                if(ans != -1 ){
-                    if(!map.containsKey(amount) || map.get(amount) > ans){
-                        map.put(amount, ans);
-                        
-                    }
+                helper(coins, amount - j * coins[i], map);
+                if(map[amount - j * coins[i]] == -1)continue;
+                if(map[amount] == 0 || (j + map[amount - j * coins[i]]) < map[amount]){
+                    map[amount] = j + map[amount - j * coins[i]];    
                 }
-                nums -= j;
+                // nums += j;
+                // int ans = helper(coins, amount - j * coins[i], map);
+                // if(ans != -1 ){
+                //     if(map[amount] == 0 || map[amount] > ans){
+                //         map[amount] = ans;
+                        
+                //     }
+                // }
+                // nums -= j;
             }
         }
-        if(!map.containsKey(amount)){
-            map.put(amount, -1);
-            return -1;
-        }else{
-            return map.get(amount);
+        
+        if(map[amount] == 0){
+            map[amount] = -1;
         }
+
+        //    return map[amount];
         
     }
 }
