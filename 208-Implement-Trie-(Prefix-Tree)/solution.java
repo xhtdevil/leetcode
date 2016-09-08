0ab7f0bc82ef875a15a8b68b7a28fee21fa48178
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-08 12:30:24 +0800

    [208. Implement Trie (Prefix Tree)][Compile Error]committed by LeetCode Extension

---
+++ 208-Implement-Trie-(Prefix-Tree)/solution.java
@@ -0,0 +1,35 @@
+class TrieNode {
+    // Initialize your data structure here.
+    public TrieNode() {
+        
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+74444
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        
+    }
+}
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie = new Trie();
+// trie.insert("somestring");
+// trie.search("key");
