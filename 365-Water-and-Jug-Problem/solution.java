a6b4f3a0889703d21e3ec385fbdabad8ddb521ad
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-22 03:22:45 +0800

    [365. Water and Jug Problem][Accepted]committed by LeetCode Extension

--- 365-Water-and-Jug-Problem/solution.java
+++ 365-Water-and-Jug-Problem/solution.java
@@ -1,40 +1,17 @@
 public class Solution {
     Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
     public boolean canMeasureWater(int x, int y, int z) {
-        if(z == 0)return true;
-        if(x == 0 && y == 0)return false;
-        if(z > x + y)return false;
-        
-        int s = Math.min(x, y);
-        int b = Math.max(x, y);
-        int sub = b - s;
-        if(z % s == 0)return true;
-        int f = s - b % s;
-        if(z == f || z == f + s || z == f + b || z == sub || z == sub + s || z == sub + b || z == s + b || z == s || z == b)return true;
-        //return help(x, y, sub, z);
-        else return false;
+       if(z > x + y)return false;
+       if(z == x || z == y || z == x + y || z == 0 || z % GCD(x, y) == 0)return true;
+       else return false;
     }
     
-    private boolean help(int a1, int a2, int a3, int sum){
-        if(map.containsKey(sum))return map.get(sum);
-        if(sum == 0)return true;
-        if(sum < 0)return false;
-        else{
-            boolean ans;
-            if((a2 == a3 || a1 == a2) && a1 != a3){
-                ans =  help(a1, a2, a3, sum - a1) || help(a1, a2, a3, sum - a3);
-            }
-            else if(a1 != a2 && a1 == a3){
-                ans = help(a1, a2, a3, sum - a1) || help(a1, a2, a3, sum - a2);
-            }
-            else if(a1 == a2 && a1 == a3){
-                ans = help(a1, a2, a3, sum - a1);
-            }
-            else {
-                ans = help(a1, a2, a3, sum - a1) || help(a1, a2, a3, sum - a2) || help(a1, a2, a3, sum - a3);
-            }
-            map.put(sum, ans);
-            return ans;
+    private int GCD(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
+        return a;
     }
 }
