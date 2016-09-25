a0dcd9d848245964911fe6da371294ded0c0b050
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-26 02:52:00 +0800

    [211. Add and Search Word - Data structure design][Accepted]committed by LeetCode Extension

--- 211-Add-and-Search-Word---Data-structure-design/solution.java
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -23,15 +23,13 @@ public class WordDictionary {
                 node.children[word.charAt(i) - 'a'] = new TrieNode(word.charAt(i));
             }
             node = node.children[word.charAt(i) - 'a'];
-            if(i != word.length() - 1)node.isWord = false;
-            else node.isWord = true;
+            if(i == word.length() - 1)node.isWord = true;
         }
     }
 
     // Returns if the word is in the data structure. A word could
     // contain the dot character '.' to represent any one letter.
     public boolean search(String word) {
-        TrieNode root = new TrieNode();
         return match(word, 0, root);
     }
     
@@ -40,14 +38,17 @@ public class WordDictionary {
         for(int i = index; i < word.length(); i++){
             if(word.charAt(i) == '.'){
                 for(int j = 0; j < 26; j++){
-                    if(node.children[j] != null)match(word, i + 1, node.children[j]);
+                    if(node.children[j] != null && match(word, i + 1, node.children[j]) == true)return true;
                 }
+                return false;
             }
-            if(node.children[word.charAt(i) - 'a'] == null)return false;
-            else node = node.children[word.charAt(i) - 'a'];
-            if(i == word.length() - 1 && node.isWord == false)return false;
+            else{
+                if(node.children[word.charAt(i) - 'a'] == null)return false;
+                else node = node.children[word.charAt(i) - 'a'];
+                if(i == word.length() - 1 && node.isWord == false)return false;
+            }
         }
-        return true;
+        return node.isWord;
     }
 }
 
