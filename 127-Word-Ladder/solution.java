f691e446ff507dd4c6bbe66631f49cc451d5e2a3
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-26 06:02:56 +0800

    [127. Word Ladder][Compile Error]committed by LeetCode Extension

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -46,7 +46,7 @@ public class Solution {
     
     private boolean oneDifference(String s1, String s2) {
         int count = 0;
-        for(int i = 0; i , s1.length(); i++){
+        for(int i = 0; i < s1.length(); i++){
             if(s1.charAt(i) != s2.charAt(i))count++;
             if(count > 1)return false;
         }
