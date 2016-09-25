0f7f75dd10ff8e2db7bc3bbb6f06acd386605dfe
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-26 02:22:00 +0800

    [211. Add and Search Word - Data structure design][Compile Error]committed by LeetCode Extension

--- 211-Add-and-Search-Word---Data-structure-design/solution.java
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -32,8 +32,18 @@ public class WordDictionary {
     // Returns if the word is in the data structure. A word could
     // contain the dot character '.' to represent any one letter.
     public boolean search(String word) {
+        TrieNode root = new TrieNode();
+        return match(word, 0, root);
+    }
+    
+    public boolean match(String word, int index, TrieNode root){
         TrieNode node = root;
-        for(int i = 0; i < word.length(); i++){
+        for(int i = index; i < word.length(); i++){
+            if(word.chatAt(i) == '.'){
+                for(int j = 0; j < 26; j++){
+                    if(node.children[j] != null)match(word, i + 1, node.children);
+                }
+            }
             if(node.children[word.charAt(i) - 'a'] == null)return false;
             else node = node.children[word.charAt(i) - 'a'];
             if(i == word.length() - 1 && node.isWord == false)return false;
