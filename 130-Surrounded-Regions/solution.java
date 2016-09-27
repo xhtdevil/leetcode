8bbeae4c50ecff2663254a824be28168f5c0fe55
xhtdevil <xuhaotian201314@yahoo.com>
2016-09-27 12:33:06 +0800

    [130. Surrounded Regions][Accepted]committed by LeetCode Extension

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -9,11 +9,11 @@ public class Solution {
             for(int j = 0; j < n ; j++){
                 if(board[0][j] == 'O'){
                     q.add(new Point(0, j));
-                    BFS(board, q);
+                    //BFS(board, q);
                 }
                 if(board[m - 1][j] == 'O'){
                     q.add(new Point(m - 1, j));
-                    BFS(board, q);
+                    //BFS(board, q);
                 }
             }
 
@@ -22,13 +22,41 @@ public class Solution {
                 if(board[i][0] == 'O'){
 
                     q.add(new Point(i, 0));
-                    BFS(board, q);
+                    //BFS(board, q);
                 }
                 if(board[i][n - 1] == 'O'){
 
                     q.add(new Point(i, n - 1));
-                    BFS(board, q);
+                    //BFS(board, q);
                 }
+        }
+        
+        while(!q.isEmpty()){
+            Point pair1 = q.poll();
+
+             int i = pair1.x, j = pair1.y;
+            board[i][j] = 'b';
+
+            if(i > 0 && board[i - 1][j] == 'O') {
+
+                q.add(new Point(i - 1, j));
+            }
+            if(i < board.length - 1 && board[i + 1][j] == 'O') {
+
+                q.add(new Point(i + 1, j));
+
+            }
+            if(j > 0 && board[i][j - 1] == 'O') {
+
+                q.add(new Point(i, j - 1));
+
+            }
+            if(j < board[0].length - 1 && board[i][j + 1] == 'O') {
+
+                q.add(new Point(i, j + 1));
+
+            }
+            
         }
         
         for(int i = 0; i < m ; i++) {
