3f7dfb1c3c00beafdea574c596669e3585318f82
xhtdevil <xuhaotian201314@yahoo.com>
2016-10-08 14:06:58 -0700

    [338. Counting Bits][Accepted]committed by LeetCode Extension

--- 338-Counting-Bits/solution.java
+++ 338-Counting-Bits/solution.java
@@ -1,30 +1,21 @@
 public class Solution {
-    public int[] countBits(int num) {
-        int nums[] = new int[num + 1];
-        for(int i = 1; i <= num; i++){
-            nums[i] = nums[i / 2] + i % 2;
-        }
-        return nums;
-        // int maxAdd = 1;
-        // int countAdd = 1;
-        // int ans = 1;
+    public int[] countBits(int n) {
+        //solution1
         // int nums[] = new int[num + 1];
-        // for(int i = 0; i <= num; i++){
-        //     if(i == 0){nums[i] = 0;continue;}
-        //     if(countAdd < maxAdd){
-        //         nums[i] = ans;
-        //         ans++;
-        //         countAdd++;
-        //     }else{
-        //         nums[i] = ans;
-        //         ans = 1;
-        //         countAdd = 1;
-        //         maxAdd = maxAdd * 2;
-        //     //    nums[i] = ans;
-        //   //     ans++;
-        //     //    countAdd++;
-        //     }
+        // for(int i = 1; i <= num; i++){
+        //     nums[i] = nums[i / 2] + i % 2;
         // }
         // return nums;
+       
+        int[] a = new int[n + 1];
+        a[0] = 0;
+        int div = 1;
+        for(int i = 1; i <= n; i++) {
+            if(i >= div * 2) {
+                div *= 2;
+            }
+            a[i] = a[i % div] + 1;        
+        }
+        return a;
     }
 }
